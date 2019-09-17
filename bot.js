20a7803c3dbe10ead2e875c83c5ab8d2d77912d7
Pinguin86 <55387905+Pinguin86@users.noreply.github.com>
2019-09-17 16:50:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,6 +38,12 @@ bot.on('message', message => {
 		if (!args.length) {
 			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
 		}
+		else if (args[0] === 'foo') {
+		return message.channel.send('bar');
+	}
+
+	message.channel.send(`First argument: ${args[0]}`);
+	},
 		message.channel.send(`Command name: ${command}\nArguments: ${args}`);
 	}
 
