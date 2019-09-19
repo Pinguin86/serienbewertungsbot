32d9a3b9242b73a4f9f3b7d59144307463a229f9
Pinguin86 <55387905+Pinguin86@users.noreply.github.com>
2019-09-19 13:45:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,11 @@ bot.on('message', message => {
 	if (!command) return;
 	
 	if(command.args && !args.length) {
-		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+		return message.channel.send(reply);
 	 }
 
 	try {
