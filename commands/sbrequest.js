2daa288258c3426250e936d54e41e80106aadcba
Pinguin86 <55387905+Pinguin86@users.noreply.github.com>
2019-09-19 13:59:51 +0200

    Create sbrequest.js

---
+++ commands/sbrequest.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'sbrequest',
+	aliases: ['request'],
+	description: 'Sendet eine Nachricht an Pinguin86 mit der Serie die bewertet werden soll.',
+	args: true,
+	usage: '<Name der Serie>',
+	execute(message, args) {
+  //message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+  bot.users.get("144189617061494784").send(`Die von ${message.author} gewünschte Serie lautet: ${args}`);
+  }
+	},
+};
