fb0758210e851e79c92e29e1bc77b2187eb7025b
Pinguin86 <55387905+Pinguin86@users.noreply.github.com>
2019-09-19 12:52:50 +0200

    Update serienbewertung.js

--- commands/serienbewertung.js
+++ commands/serienbewertung.js
@@ -5,8 +5,9 @@ module.exports = {
 		if (!args.length) {
 			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
 		} else if (args[0] === 'happy') {
-			return message.channel.send('```verarbeite: !Pinguin S\ntesttest```');
-			return message.channel.send('```verarbeite: !Pinguin S\ntesttest```');
+			return message.channel.send('```verarbeite: !Pinguin S\n```+
+						    ```testtest```');
+			
 		}
 		else if (args[0] === 'bar' && args[1]=== 'foo') {
 			return message.channel.send('bar bar');
