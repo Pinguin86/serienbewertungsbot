5c03b13f8e8dc858fa6f0cb630e03424704ef062
Pinguin86 <55387905+Pinguin86@users.noreply.github.com>
2019-09-19 13:15:17 +0200

    Update serienbewertung.js

--- commands/serienbewertung.js
+++ commands/serienbewertung.js
@@ -4,8 +4,8 @@ module.exports = {
 	execute(message, args) {
 		if (!args.length) {
 			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-		} else if (args[0].toLowerCase() === 'happy') {
-			return message.channel.send('```verarbeite: !Pinguin Serienbewertung "HAPPY" \n'+
+		} else if (args[0].toLowerCase() === 'happy' || args[0].toLowerCase() === 'happy!' ) {
+			return message.channel.send('```verarbeite: !Pinguin Serienbewertung "HAPPY!" \n'+
 						    'Serie beinhaltet: Christopher Meloni, Lili Mironjnick (how the fuck do you spell your name?), Christopher Fitzgerald \n'+
 						    'Viel Gewalt in der Serie. \n'+
 						    'Wurde durch die viele Gewalt viel besser. \n'+
@@ -18,10 +18,14 @@ module.exports = {
 				                    'Unbedingt sehen!```');
 			
 		}
-		else if (args[0] === 'bar' && args[1]=== 'foo') {
-			return message.channel.send('bar bar');
-		}
-
-		message.channel.send(`First argument: ${args[0]}`);
-	},
+		} else if (args[0].toLowerCase() === 'better' && args[1].toLowerCase() === 'than' && args[3].toLowerCase() === 'us') {
+			return message.channel.send('```verarbeite: !Pinguin Serienbewertung "Better Than Us" \n'+
+						    'Serie beinhaltet: random Russische Schauspieler wie Paulina Andreeva, Kirill Käro, Eldar Kalimulin und Vera Panfilova \n'+
+						    'Gute Idee: Sexroboter hat Gefühle, ein Gewissen und denkt selbstständig, findet "Familie" in Form von zwei Kindern, einem Mann und einer Ehefrau die mit ihrem Geliebten flüchten will \n'+
+						    'Semigute Umsetzung: Zu wenig Sex mit dem Roboter; obwohl der Sexbot teilweise selbstständig denkt und handelt, kommt die Diskussion "Wie wichtig/hilfreich/gefährlich sind selbstdenkende Roboter" nur nebenbei auf;  \n'+
+						    'Nebenhandlungen und Nebencharaktere nicht total dumm \n'+
+						    'Daher Empfehlung: Nur eine Staffel verfügbar, kann man sich ansehen solange man nicht zuviel Tiefgang erwartet.  \n'+
+						    'Erfüllt nicht die Erwartungen von Pinguin, trotzdem besser als "Blacklist"```');
+			
+		},
 };
