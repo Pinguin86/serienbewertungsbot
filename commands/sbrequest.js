e3a00835f20b5dea12b6868949b89d66a38a85f5
Pinguin86 <55387905+Pinguin86@users.noreply.github.com>
2019-09-19 18:59:53 +0200

    Update sbrequest.js

--- commands/sbrequest.js
+++ commands/sbrequest.js
@@ -11,7 +11,7 @@ module.exports = {
  		//return bot.fetchUser("144189617061494784").then((user) =>{ user.send(`Die von ${message.author} gewünschte Serie lautet: ${args}`);   })	
 		//return message.bot.users.get("144189617061494784").send(`Die von ${message.author} gewünschte Serie lautet: ${args}`);
 		// return message.channel.send(`Die von ${message.author} gewünschte Serie lautet: ${args}`);		
-		return bot.users.fetch("144189617061494784").then((user) =>{ user.send(`Die von ${message.author} gewünschte Serie lautet: ${args}`);   })	
+		/*return*/ bot.users.fetch("144189617061494784").then((user) =>{ user.send(`Die von ${message.author} gewünschte Serie lautet: ${args}`);   })	
 
 	},
 };
