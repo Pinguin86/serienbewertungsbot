b0549164411d5fb0e90c98ee8c15e288210a8901
Pinguin86 <55387905+Pinguin86@users.noreply.github.com>
2019-09-19 19:52:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ bot.on('message', message => {
 	 }
 
 	try {
-	command.execute(bot, message, args);
+	command.execute(message, args);
 		} catch (error) {
 			console.error(error);
 			message.reply('there was an error trying to execute that command!');
