2b938fe6e6f81b1b027f7f730c9914f31eee4b89
Pinguin86 <55387905+Pinguin86@users.noreply.github.com>
2019-09-19 19:47:16 +0200

    Update sbrequest.js

--- commands/sbrequest.js
+++ commands/sbrequest.js
@@ -4,7 +4,7 @@ module.exports = {
 	description: 'Sendet eine Nachricht an Pinguin86 mit der Serie die bewertet werden soll.',
 	args: true,
 	usage: '<Name der Serie>',
-	execute(/*bot,*/ message, args) {
+	execute(bot, message, args) {
 	
  	 //message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
 	//	bot.users.get("YOUR ID HERE").sendMessage
