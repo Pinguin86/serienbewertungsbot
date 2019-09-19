c29d5d0101a01e908011525ebd49cfa106b6d0d2
Pinguin86 <55387905+Pinguin86@users.noreply.github.com>
2019-09-19 18:57:48 +0200

    Update sbrequest.js

--- commands/sbrequest.js
+++ commands/sbrequest.js
@@ -8,8 +8,10 @@ module.exports = {
  	 //message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
 	//	bot.users.get("YOUR ID HERE").sendMessage
 		//fetchUser('123456789').then((user) => {    user.send("My Message");
- 		return bot.fetchUser("144189617061494784").then((user) =>{ user.send(`Die von ${message.author} gewünschte Serie lautet: ${args}`);   })	
+ 		//return bot.fetchUser("144189617061494784").then((user) =>{ user.send(`Die von ${message.author} gewünschte Serie lautet: ${args}`);   })	
 		//return message.bot.users.get("144189617061494784").send(`Die von ${message.author} gewünschte Serie lautet: ${args}`);
 		// return message.channel.send(`Die von ${message.author} gewünschte Serie lautet: ${args}`);		
+		return bot.users.fetch("144189617061494784").then((user) =>{ user.send(`Die von ${message.author} gewünschte Serie lautet: ${args}`);   })	
+
 	},
 };
