e4a0bd69929fd52902b1884f17e51bc9ee9ebd9a
Pinguin86 <55387905+Pinguin86@users.noreply.github.com>
2019-09-19 12:58:12 +0200

    Update serienbewertung.js

--- commands/serienbewertung.js
+++ commands/serienbewertung.js
@@ -5,8 +5,11 @@ module.exports = {
 		if (!args.length) {
 			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
 		} else if (args[0] === 'happy') {
-			return message.channel.send('```verarbeite: !Pinguin S\n'+
-						    'testtest```');
+			return message.channel.send('```verarbeite: !Pinguin Serienbewertung "HAPPY" \n'+
+						    'Serie beinhaltet: Christopher Meloni, Lili Mironjnick (how the fuck do you spell your name?), Christopher Fitzgerald \n'+
+						    'Viel Gewalt in der Serie. \n'+
+						    'Wurde durch die viele Gewalt viel besser. \n'+
+						    'Christopher Meloni spielt einen heruntergekommenen Polizisten der ein gekidnapptes Mädchen retten will.```');
 			
 		}
 		else if (args[0] === 'bar' && args[1]=== 'foo') {
