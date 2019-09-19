9e4541df46a298e95902038bedd484f94a7accf3
Pinguin86 <55387905+Pinguin86@users.noreply.github.com>
2019-09-19 22:57:44 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,6 @@
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
-	aliases: ['time'],
 	execute(message, args) {
 		message.channel./*send('Pong.');*/send(new Date().getTime() - message.createdTimestamp + " ms...btw: Pong.");
 	},
